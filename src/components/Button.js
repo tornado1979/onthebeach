52f44be9678c868c5695fb7970bc0ea7d7a9e9ad
Theodoros Itzaris <theodoreme@gmail.com>
2019-09-03 10:07:57 +0100

    Add 'Button' component

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  return (
+    <button className="button--primary">
+      <span className="text-line1">{props.textLine1}</span>
+      <span className="text-line2">{props.textLine2}</span>
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  textLine1: 'Book now',
+  textLine2: '£ 1,300.00'
+};
+
+Button.propTypes = {
+  textLine1: PropTypes.string,
+  textLine2: PropTypes.string
+};
+
+export default Button;
