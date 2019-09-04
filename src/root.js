9619ce5961c6d8451e846a7016b87fc62877606c
Theodoros Itzaris <theodoreme@gmail.com>
2019-09-04 13:44:23 +0100

    Remove about page

--- src/root.js
+++ src/root.js
@@ -5,19 +5,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 import Home from './pages/home';
-import About from './pages/about';
 import Header from './components/Header';
 import history from './helpers/history';
 
 const Root = ({ store }) => (
    <Provider store={store}>
     <Router history={history}>
-      <div className="wrapper">
-        <Header />
-        <div className="main">
-          <Route component={Home} exact path="/" />
-          <Route component={About} exact path="/about" />
-        </div>
+      <Header />
+      <div className="main">
+        <Route component={Home} exact path="/" />
       </div>
     </Router>
     </Provider>
